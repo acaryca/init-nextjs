22b728dd1be02e2fb5e7d05967922423e5bf32a8
HyKo <punk.flag8609@fastmail.com>
2025-04-19 15:29:38 -0400

    Enhance layout in cli.js by adding support for ACARY_STATS_URL and ACARY_STATS_ID environment variables, and update body class for improved styling. Add new environment variables to .env file for better configuration management.

--- cli.js
+++ cli.js
@@ -130,17 +130,20 @@ async function initProject() {
 			'./src/app/layout.js',
 		]);
 
-		const layoutContent = `import "@styles/globals.css";
+		const layoutContent = `import Script from "next/script";
+		
+import "@styles/globals.css";
 
 export const metadata = {
 	title: "ACARY",
 	description: "ACARY",
 };
 
-export default function RootLayout({ children }) {
+export default function layout({ children }) {
 	return (
 		<html>
-			<body>
+			<body className="antialiased">
+				{process.env.ACARY_STATS_URL && process.env.ACARY_STATS_ID && (<Script src={process.env.ACARY_STATS_URL} data-website-id={process.env.ACARY_STATS_ID} defer />)}
 				{children}
 			</body>
 		</html>
